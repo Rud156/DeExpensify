121765fd95e531c69f1481cfde81b8a91ffea827
Rud156 <rudrangshu_1997@yahoo.com>
2018-06-02 08:33:50 +0530

    Added Experimental Modal

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
-import { Container, Header, Text, Body, Title, Content, Card, CardItem } from 'native-base';
+import { Container, Header, Text, Body, Title, Content, Card, CardItem, Button } from 'native-base';
+import { Modal } from 'react-native';
 
 import { COLORS } from '../utils/Constants';
 
-class Profile extends React.Component {
+interface Props {}
+interface State {
+  modalOpen: boolean;
+}
+
+class Profile extends React.Component<Props, State> {
   static navigatorStyle = {
     navBarHidden: true,
   };
 
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      modalOpen: false,
+    };
+  }
+
+  openModal = () => {
+    this.setState({ modalOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalOpen: false });
+  };
+
   render() {
+    const { modalOpen } = this.state;
     return (
       <Container>
         <Header style={{ backgroundColor: COLORS.BLUE }}>
@@ -17,11 +40,27 @@ class Profile extends React.Component {
           </Body>
         </Header>
         <Content padder>
+          <Modal
+            presentationStyle="pageSheet"
+            animationType="slide"
+            transparent={false}
+            visible={modalOpen}
+            onRequestClose={this.closeModal}
+          >
+            <Header style={{ backgroundColor: COLORS.BLUE }}>
+              <Body>
+                <Title>Modal</Title>
+              </Body>
+            </Header>
+          </Modal>
           <Card>
             <CardItem header bordered>
               <Text>Profile</Text>
             </CardItem>
           </Card>
+          <Button onPress={this.openModal}>
+            <Text>Open Modal</Text>
+          </Button>
         </Content>
       </Container>
     );
